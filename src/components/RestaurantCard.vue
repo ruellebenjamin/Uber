84925bc2e0a2641b0fe06bff8c46a1537dd5cd9d
Benjamin Ruelle <bnji@extz.home>
2024-09-19 11:52:08 +0200

    first commit

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["restaurant"])
+</script>
+
+<template>
+  <div class=" justify-around ">
+    <img class="w-72" src="https://media.istockphoto.com/id/936176018/fr/photo/tacos-de-boeuf-servis-avec-frites-dorées-de-français.jpg?s=612x612&w=0&k=20&c=qzwV8BwE8Le4R2pRxFl2XGK_8PU0b7VCiRmcIEesfrQ=" alt="">
+    <h2>{{restaurant.name}}</h2>
+    <p>{{ restaurant.description }} </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import RestaurantCard from "@/components/RestaurantCard.vue";
+import axios from "axios";
+
+const restaurants = [
+  { name: "O'Tacos", description: "Le tacos" },
+  { name: "La ruche", description: "La ruche de troyes" },
+  { name: "Burger King", description: "Le BK" }
+]
+
+
+
+const getRestaurant = async () => {
+  const response = await axios.get('http://localhost:8215/restaurants');
+  restaurants.value = response.data;
+
+  console.log(response);
+}
+</script>
+
+<template>
+
+
+  <div class="flex flex-wrap gap-4">
+<RestaurantCard v-for="restaurant in restaurants" :restaurant="restaurant"/>
+  </div>
+</template>
